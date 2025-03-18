43304aad5de762a97f60f41164b5d4228110b450
tamz11 <minhtam212005@gmail.com>
2025-03-18 23:53:33 +0700

    project1

---
+++ Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './Counter.css';
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Counter App</h2>
+      <p>Giá trị: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Tăng</button>
+      <button onClick={() => setCount(count - 1)}>Giảm</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
